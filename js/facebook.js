a9afbf48c04133953cbf9e9bfbb9f645db6356d7
Reed La Botz <reedlabotz@gmail.com>
2011-07-05 22:26:30 -0500

    some stuff kinda works

--- js/facebook.js
+++ js/facebook.js
@@ -1,7 +1,10 @@
+$(".a-la-h-ga:last").after("Facebook");
+
+
 chrome.extension.sendRequest({'action' : 'getFacebook'},function(data){
    console.log("We have facebook in google+");
    var dataSplit = data.split("for (;;);");
    var goodData = dataSplit[1];
    var jsonData = $.parseJSON(goodData);
-   $("#content").html(jsonData.payload);
+   //$("#content").html(jsonData.payload);
 });
